fee57474a55f0972c4c69c7d452c6117d92be7cb
JavSalv <javiersalvadorpenacoba@gmail.com>
2023-11-16 20:30:43 +0100

    Comentarios

--- chunk.h
+++ chunk.h
@@ -5,26 +5,35 @@
 #include "value.h"
 
 
+
 typedef enum{
     OP_CONSTANT,
+    OP_CONSTANT_LONG,
     OP_RETURN
 }OpCode;
 
-//Un chunk representa un conjunto de bytecodes (un mismo scope?).
-//Count se corresponde con el número de bytes/instrucciones que contiene;
-//Capacity es el número de bytes que caben en el chunk;
-//En constants se almacenan las constantes del chunk (enteros, strings, etc).
+/// @brief Representa un conjunto de bytecodes (un mismo scope?).
 typedef struct {
-    int count;
-    int capacity;
-    uint8_t* code;
-    int* lines;
-    ValueArray constants;
+    int count;              //Numero de bytes/instrucciones que contiene.
+    int capacity;           //Capacity es el número de bytes que caben en el chunk.
+    uint8_t* code;          //Las instrucciones que contiene.
+    int* lines;             //Correspondencia entre lineas reales y bytecodes.
+    ValueArray constants;   //Constantes del chunk: numeros, strings, etc.
 }Chunk;
 
+
+/// @brief Inicializa un chunk vacío.
+/// @param chunk Chunk a inicializar.
 void initChunk(Chunk* chunk);
+
+/// @brief Escribe un bytecode en el chunk.
+/// @param chunk Chunk.
+/// @param byte Byte a escribir??
+/// @param line Linea que contiene la instruccion que genera el bytecode.
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
+
 int addConstant(Chunk* chunk, Value value);
+
 void freeChunk(Chunk* chunk);
 
 
--- debug.h
+++ debug.h
@@ -3,7 +3,16 @@
 
 #include "chunk.h"
 
+
+/// @brief Desensambla un chunk.
+/// @param chunk chunk a desensamblar.
+/// @param name nombre para identificar al chunk.
 void disassembleChunk(Chunk* chunk, const char* name);
+
+/// @brief Desensambla una instrucción dentro de un chunk.
+/// @param chunk Chunk que contiene la instrucción.
+/// @param offset Offset de la instrucción dentro del chunk.
+/// @return Offset de la siguiente instruccion a desensamblar
 int disassembleInstruction(Chunk* chunk, int offset);
 
 #endif
